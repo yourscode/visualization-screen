ba18e87c07965db8472e402f6cc5be84bcd6e52d
yourscode <415943594@qq.com>
2020-01-16 15:30:14 +0800

    添加标题

--- js/index.js
+++ js/index.js
@@ -1190,3 +1190,152 @@ option_gauge = {
     }]
 };
 myCharts_gauge.setOption(option_gauge)
+
+// linearBar
+var myCharts_linearBar = echarts.init(document.getElementById("linearBar"))
+var colorArray = [
+    {
+        top: '#ffa800', //黄
+        bottom: 'rgba(11,42,84,.3)'
+    }, {
+        top: '#1ace4a', //绿
+        bottom: 'rgba(11,42,84, 0.3)'
+    },
+    {
+        top: '#4bf3ff', //蓝
+        bottom: 'rgba(11,42,84,.3)'
+    }, {
+        top: '#4f9aff', //深蓝
+        bottom: 'rgba(11,42,84,.3)'
+    },
+    {
+        top: '#b250ff', //粉
+        bottom: 'rgba(11,42,84,.3)'
+    }
+];
+option_linearBar = {
+    backgroundColor: '#0E2A43',
+     tooltip: {
+          show: true,
+          formatter: "{b}:{c}"
+        },
+   grid: {
+          left: '5%',
+          top: '12%',
+          right: '1%',
+          bottom: '8%',
+          containLabel: true
+        },
+  
+    xAxis: {
+        type: 'value',
+        show:false,
+        position: 'top',
+        axisTick: {
+            show: false
+        },
+        axisLine: {
+            show: false,
+            lineStyle: {
+                color: '#fff',
+            }
+        },
+        splitLine: {
+            show: false
+        },
+    },
+    yAxis: [{
+            type: 'category',
+            axisTick: {
+                show: false,
+                alignWithLabel: false,
+                length: 5,
+
+            },
+            "splitLine": { //网格线
+                "show": false
+            },
+            inverse: 'true', //排序
+            axisLine: {
+                show: false,
+                lineStyle: {
+                    color: '#fff',
+                }
+            },
+            data: ['车辆', '民警', '武警', '特警', '交巡警']
+        }
+
+    ],
+    series: [{
+            name: '能耗值',
+            type: 'bar',
+                label: {
+                normal: {
+                  show: true,
+                  position: 'right',
+                  formatter: '{c}',
+                  textStyle: {
+                    color: 'white' //color of value
+                  }
+                }
+              },
+            itemStyle: {
+                normal: {
+                    show: true,
+                    color: function(params) {
+                        let num = colorArray.length;
+                        return {
+                            type: 'linear',
+                            colorStops: [{
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }, {
+                                offset: 0,
+                                color: colorArray[params.dataIndex % num].bottom
+                            }, {
+                                offset: 1,
+                                color: colorArray[params.dataIndex % num].top
+                            }],
+                            //globalCoord: false
+                        }
+                    },
+                    barBorderRadius: 70,
+                    borderWidth: 0,
+                    borderColor: '#333',
+                }
+            },
+            barGap: '0%',
+            barCategoryGap: '50%',
+            data: [50, 100, 30, 200, 100]
+        }
+
+    ]
+};
+myCharts_linearBar.setOption(option_linearBar)
